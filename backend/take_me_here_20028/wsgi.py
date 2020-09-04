a97c52e35eb3f1bfe8cad8d2be43ab9cb1902737
Crowdbotics <team@crowdbotics.com>
2020-09-04 19:35:09 +0000

    Update models

--- backend/take_me_here_20028/wsgi.py
+++ backend/take_me_here_20028/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'take_me_here_20028.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "take_me_here_20028.settings")
 
 application = get_wsgi_application()
